0efbc747c27da4fb662230817ccf08018ae33087
Joshua Salinas <joshuasalinashernandez@gmail.com>
2021-12-22 14:16:51 -0600

    addding new updates

---
+++ Commander/Data/ICommanderRepo.cs
@@ -0,0 +1,8 @@
+using Commander.Models;
+
+namespace Commander.Data{
+    public interface ICommanderRepo{
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int id);
+    }
+}
---
+++ Commander/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Commander{
+    public class Startup{
+        
+        public Startup(IConfiguration configuration){
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration{ get;}
+
+        public void ConfigureServices(IServiceCollection services){
+            services.AddControllers();
+        }
+
+         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //ADDED AFTER TUTORIAL
+            app.UseSwagger();
+
+            //ADDED AFTER TUTORIAL
+            app.UseSwaggerUI( c=> {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Commander API V1");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
